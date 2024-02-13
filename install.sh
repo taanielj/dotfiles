69bac553a216a75e4a88af3838c16057162ce4b9
taanielj <taaniel.jakobson@gmail.com>
2024-02-13 18:35:40 +0200

    small fixes in install script

--- install.sh
+++ install.sh
@@ -6,80 +6,74 @@
 # ZSH
 
 check_root() {
-    if [ "$EUID" -ne 0 ]; then
-        echo "Please run as root"
-        exit 1
-    fi
+	if [ "$EUID" -ne 0 ]; then
+		SUDO='sudo'
+	else
+		SUDO=''
+	fi
 }
 
-check_apt() {
-    if [ -x "$(command -v apt-get)" ]; then
-        echo "apt-get found"
-    else
-        echo "apt-get not found"
-        exit 1
-    fi
+# get user home (~) if not root
+if [ -z "$SUDO" ]; then
+	USER_HOME=$HOME
+else
+	USER_HOME=$(eval echo ~$(logname))
+fi
+
+update_packages() {
+	if [ ! -x "$(command -v apt-get)" ]; then
+		echo "apt-get not found"
+		exit 1
+	else
+		$SUDO apt-get update
+	fi
+
 }
 
-check git() {
-    if [ -x "$(command -v git)" ]; then
-        echo "git found"
-    else
-        echo "git not found"
-        exit 1
-    fi
+check_git() {
+	if [ ! -x "$(command -v git)" ]; then
+		echo "Git not found, installing git"
+		$SUDO apt-get install -y git
+	fi
 }
-
-change_shell() {
-    echo "Would you like to change your shell to zsh? (y/n)"
-    read answer
-    if [ "$answer" == "y" ]; then
-        chsh -s $(which zsh)
-        echo "Shell changed to zsh"
-    else
-        echo "Shell not changed"
-    fi
-    zsh
-}
-
 
 install_zsh() {
-    echo "Installing zsh"
-    apt-get install -y zsh
-    # get_oh_my_zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    # get auto-suggestions
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    cp .zshrc ~/.zshrc
-    source ~/.zshrc
+	echo "Installing zsh"
+	$SUDO apt-get install -y zsh
+	# install oh-my-zsh from github
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	# get auto-suggestions
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$USER_HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	cp .zshrc /.zshrc
 }
-source ~/.zshrc
 
 install_tmux() {
-    echo "Installing tmux"
-    apt-get install -y tmux
-    cp .tmux.conf ~/.tmux.conf
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    # start tmux and install plugins
-    tmux start-server
-    tmux new-session -d
-    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-    tmux kill-server
+	echo "Installing tmux"
+	$SUDO apt-get install -y tmux
+	cp .tmux.conf $USER_HOME/.tmux.conf
+	git clone https://github.com/tmux-plugins/tpm $USER_HOME/.tmux/plugins/tpm
+	# start tmux and install plugins
+	tmux start-server
+	tmux new-session -d
+	$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
+	tmux kill-server
 }
 
 install_nvim() {
-    echo "Installing neovim"
-    wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
-    chmod u+x nvim.appimage
-    mv nvim.appimage /usr/local/bin/nvim
-    mv nvim/ ~/.config/nvim
+	echo "Installing neovim"
+	wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
+	chmod u+x nvim.appimage
+	$SUDO mv nvim.appimage /usr/local/bin/nvim
+	$SUDO mv nvim/ $USER_HOME/.config/nvim
 }
 
 check_root
-check_apt
+update_packages
 check_git
-apt-get update || { echo "apt-get update failed"; exit 1;}
-install_zsh
+$SUDO apt-get update || {
+	echo "apt-get update failed"
+	exit 1
+}
 install_tmux
 install_nvim
-change_shell
+
