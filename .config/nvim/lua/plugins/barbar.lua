84af5c456fea38958c1c26b418c3298143f7ef5c
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2024-02-18 21:50:12 +0200

    linted+checkpoint

---
+++ .config/nvim/lua/plugins/barbar.lua
@@ -0,0 +1,16 @@
+return {
+    "romgrk/barbar.nvim",
+    dependencies = {
+        "lewis6991/gitsigns.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        vim.g.barbar_auto_setup = false
+        require'barbar'.setup {
+            sidebar_filetypes = {
+                NvimTree = true,
+                ['neo-tree'] = {event = 'BufWipeout'},
+            },
+        }
+    end,
+}
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,12 +1,28 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+				},
+			})
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		dependencies = { "nvimtools/none-ls.nvim", "williamboman/mason.nvim" },
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end,
+	},
 }
