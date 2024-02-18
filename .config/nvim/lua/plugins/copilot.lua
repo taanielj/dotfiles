9ae00d796528202fd0f4ba9ae500148e56e7cbde
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2024-02-18 19:57:00 +0200

    linted+checkpoint

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,23 +1,21 @@
 return {
-    -- {
-    -- -- use alternative copilot plugin that uses lua not vimscript
-    -- "zbirenbaum/copilot.lua",
-    -- cmd = "Copilot",
-    -- event = "InsertEnter",
-    -- config = function()
-    --     require("copilot").setup({
-    --         -- disable suggestions by this plugin, they will be handled by copilot-cmp (the next plugin in this lua table)
-    --         suggestion = { enabled = false },
-    --         panel = { enabled = false },
-    --     })
-    -- end
-    -- },
-    -- {
-    --     "zbirenbaum/copilot-cmp",
-    --     config = function()
-    --         require("copilot_cmp").setup()
-    --     end
-    -- }
+	-- {
+	-- -- use alternative copilot plugin that uses lua not vimscript
+	-- "zbirenbaum/copilot.lua",
+	-- cmd = "Copilot",
+	-- event = "InsertEnter",
+	-- config = function()
+	--     require("copilot").setup({
+	--         -- disable suggestions by this plugin, they will be handled by copilot-cmp (the next plugin in this lua table)
+	--         suggestion = { enabled = false },
+	--         panel = { enabled = false },
+	--     })
+	-- end
+	-- },
+	-- {
+	--     "zbirenbaum/copilot-cmp",
+	--     config = function()
+	--         require("copilot_cmp").setup()
+	--     end
+	-- }
 }
-    
-    
