9fff8eb5a92a0e33b4788a63114bc47fadbfdfda
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2024-02-19 21:45:00 +0200

    added bracket closing

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -21,3 +21,5 @@ vim.keymap.set("n", "<C-j>", ":m .+1<CR>==", { noremap = true, silent = true, de
 vim.keymap.set("n", "<C-k>", ":m .-2<CR>==", { noremap = true, silent = true, desc = "Move line up" })
 vim.keymap.set("n", "<C-Up>", ":m .-2<CR>==", { noremap = true, silent = true, desc = "Move line up" })
 vim.keymap.set("n", "<C-Down>", ":m .+1<CR>==", { noremap = true, silent = true, desc = "Move line down" })
+
+vim.keymap.set("n", "<Tab>", "<C-w>w", { noremap = true, silent = true, desc = "Next window"})
