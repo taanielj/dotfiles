f775ce2eb261443fc0a419093a01894b37de06d4
taanielj <taaniel.jakobson@gmail.com>
2024-02-19 15:50:30 +0200

    set timeout to 0

--- .config/nvim/lua/plugins/whick-key.lua
+++ .config/nvim/lua/plugins/whick-key.lua
@@ -2,6 +2,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     config = function()
+        vim.o.timeoutlen = 0
         local wk = require("which-key")
         wk.register({
             f = {
