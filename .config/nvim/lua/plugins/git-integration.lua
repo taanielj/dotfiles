19fb2b49c5d05898b579da7e9894f13915c80a20
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2024-02-19 20:47:49 +0200

    changed git icons

--- .config/nvim/lua/plugins/git-integration.lua
+++ .config/nvim/lua/plugins/git-integration.lua
@@ -1,10 +1,13 @@
 return {
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Changes" })
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Blame" })
+		end,
+	},
     {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require("gitsigns").setup()
-            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Changes" })
-            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Blame" })
-        end,
+        "tpope/vim-fugitive",
     }
 }
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,33 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left toggle=true<CR>", {desc = "Show Files", silent = true})
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left toggle=true<CR>", { desc = "Show Files", silent = true })
+
+		require("neo-tree").setup({
+			default_component_configs = {
+				git_status({
+					symbols = {
+						-- Change type
+						added = "✚", -- NOTE: you can set any of these to an empty string to not show them
+						deleted = "✖",
+						modified = "",
+						renamed = "➜",
+						-- Status type
+						untracked = "",
+						ignored = "◌",
+						unstaged = "",
+						staged = "✓",
+						conflict = "",
+					},
+                    align = "right",
+				}),
+			},
+		})
+	end,
 }
