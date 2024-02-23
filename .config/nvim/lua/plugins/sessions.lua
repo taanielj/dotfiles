eb227bee5d3a0e9bd4f4b429589e016b23be1d7c
taanielj <taaniel.jakobson@gmail.com>
2024-02-23 12:20:48 +0200

    checkpoint

---
+++ .config/nvim/lua/plugins/sessions.lua
@@ -0,0 +1,11 @@
+
+return {
+--	"rmagatti/auto-session",
+--	config = function()
+--		require("auto-session").setup({
+--			pre_save_cmds = {
+--                { "Neotree close" }
+--			},
+--		})
+--	end,
+}
