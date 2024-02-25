07f8368506149d00f052f15c3f48b5d4e6087d35
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2024-02-25 14:27:18 +0200

    added lazygit

--- install.sh
+++ install.sh
@@ -139,6 +139,15 @@ install_nvim() {
     ln -s $repo_dir/.config/nvim $HOME/.config/nvim
 }
 
+install_lazygit() {
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin/
+    # Clean up
+    rm lazygit.tar.gz lazygit
+}
+
 
 
 
@@ -156,5 +165,6 @@ configure_zsh
 configure_tmux
 install_nvim
 install_rust_stuff
+install_lazygit
 
 echo "Installation completed successfully."
