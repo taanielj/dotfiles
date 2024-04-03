050d1d324810c8aaf4d6d12e5b964170ddfdfba7
taanielj <taaniel.jakobson@gmail.com>
2024-04-03 11:46:06 +0300

    added realtime

--- .p10k.zsh
+++ .p10k.zsh
@@ -1710,3 +1710,5 @@ typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
 (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
 'builtin' 'unset' 'p10k_config_opts'
+
+typeset -g POWERLEVEL9K_EXPERIMENTAL_TIME_REALTIME=true
