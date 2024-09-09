793362fac9fdf87d252a8e9e292e495b01d42214
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2024-09-09 11:20:30 +0300

    updated

--- .config/nvim/lua/plugins/whick-key.lua
+++ .config/nvim/lua/plugins/whick-key.lua
@@ -1,33 +1,22 @@
 return {
 	"folke/which-key.nvim",
+	dependencies = {
+		"echasnovski/mini.icons",
+	},
 	event = "VeryLazy",
 	config = function()
 		vim.o.timeoutlen = 200
 		local wk = require("which-key")
-		wk.register({
-			f = {
-				name = "+Find",
-				h = {
-					name = "+Hidden",
-				},
-			},
-			g = {
-				name = "+Git",
-			},
 
-            b = {
-                name = "+Buffer",
-                {
-                    s = "+Sort",
-                },
-                {
-                    m = "+Move",
-                },
-            },
-            l = {
-                name = "+LSP"
-            },
-		}, { prefix = "<leader>" })
+		wk.add({
+			{ "<leader>b", group = "Buffer" },
+			{ "<leader>b s", "Sort" },
+			{ "<leader>b m", "Move" },
+			{ "<leader>l", "LSP" },
+			{ "<leader>f", group = "Find" },
+			{ "<leader>f h", "Hidden" },
+			{ "<leader>g", "Git" },
+		})
 
 		require("which-key").setup()
 	end,
--- install.sh
+++ install.sh
@@ -137,6 +137,12 @@ install_nvim() {
     ln -s $repo_dir/.config/nvim $HOME/.config/nvim
 }
 
+install_lazygit() {
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+}
 
 install_fastfetch(){
     sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
