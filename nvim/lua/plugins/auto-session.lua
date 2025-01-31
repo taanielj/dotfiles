b6c121932ea6fb1e9af23facd294eb0511a495ff
Taaniel Jakobson <taaniel.jakobson@zendesk.com>
2025-01-31 09:00:00 +0200

    added auto-session

--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -1,46 +1,75 @@
 return {
-	"rmagatti/auto-session",
-	lazy = false,
+    "rmagatti/auto-session",
+    lazy = false,
 
-	config = function()
-		require("auto-session").setup({
-			atuo_restore_last_session = true,
-			auto_save_enabled = true,
-			auto_create = function()
-				local cmd = "git rev-parse --is-inside-work-tree"
-				return vim.fn.system(cmd) == "true\n"
-			end,
-			auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
-			auto_session_enabled_filetypes = {
-				"dashboard",
-				"alpha",
-			},
-			suppressed_dirs = {
-				"/tmp",
-				"/private/tmp",
-				"/var/tmp",
-				"/usr/tmp",
-				"/usr/local/tmp",
-				"~/",
-				"/",
-				"~/git",
-				"~/Downloads",
-				"~/Desktop",
-			},
-			bypass_save_filetype = {
-				"dashboard",
-				"alpha",
-			},
-			cwd_change_handling = true,
-			pre_cwd_changed_cmds = {
-				"tabdo Neotree action=close",
-			},
+    config = function()
+        require("auto-session").setup({
+            enabled = true,
+            auto_clean_after_session_restore = false,
+            auto_save_enabled = true,
+            auto_create = function()
+                local cmd = "git rev-parse --is-inside-work-tree"
+                return vim.fn.system(cmd) == "true\n"
+            end,
+            auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
+            close_unsupported_windows = true,
+            suppressed_dirs = {
+                "/tmp",
+                "/private/tmp",
+                "/var/tmp",
+                "/usr/tmp",
+                "/usr/local/tmp",
+                "~/",
+                "/",
+                "~/git",
+                "~/Downloads",
+                "~/Desktop",
+            },
+            bypass_save_filetypes = {
+                "dashboard",
+                "alpha",
+                "neo-tree",
+            },
+            cwd_change_handling = true,
+            pre_cwd_changed_cmds = {
+                "tabdo Neotree action=close",
+            },
             post_cwd_changed_cmds = {
                 "tabdo Neotree",
             },
             post_restore_cmds = {
-                "tabdo Neotree",
+                function()
+                    local bufnr_list = vim.api.nvim_list_bufs()
+                    local only_no_name = true
+                    for _, bufnr in ipairs(bufnr_list) do
+                        if vim.api.nvim_buf_get_name(bufnr) ~= "" then
+                            only_no_name = false
+                            break
+                        end
+                    end
+                    if only_no_name then
+                        vim.cmd("%bd!")
+                        vim.cmd("Alpha")
+                        vim.cmd("tabdo bd#")
+                    else
+                        vim.cmd("Neotree")
+                    end
+                end
             },
-		})
-	end,
+            pre_delete_cmds = {
+                "Neotree action=close",
+                -- save all buffers
+                "silent! wa",
+                "silent! %bd!",
+            },
+            post_delete_cmds = {
+                function()
+                    return {
+                        vim.cmd("Alpha"),
+                        vim.cmd("bd#"),
+                    }
+                end
+            }
+        })
+    end,
 }
