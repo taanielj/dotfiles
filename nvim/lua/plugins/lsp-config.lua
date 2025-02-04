2007ecd0f8f8597ff753f38e25aa3601e50e57d0
Taaniel Jakobson <taaniel.jakobson@zendesk.com>
2025-02-04 09:54:49 +0200

    added automatic linter install with mason

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,105 +1,126 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					-- lua
-					"lua_ls", -- lsp
-					"stylua", -- formatter/linter
-					-- Python
-					-- "pylsp", -- lsp
-					"pyright", -- lsp/static type checker 
-					"black", -- formatter/linter
-                    -- Docker and Terraform language servers
-					"dockerls",
-					"terraformls",
-                    -- SQL
-                    -- TODO: Configure SQL language servers and formatters properly for specific sql dialects
-					"sqlls", -- language server
-                    "sqlfmt", -- formatter
-                    -- HTML, CSS, and JavaScript
-					"html",
-					"eslint",
-					"cssls",
-					-- bash
-					"bashls", -- language server
-					"shfm", -- formatter/linter
-					-- go
-					"gopls", -- official go language server for LSP supporting editors
-					"golangci-lint", -- formatter/linter
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    -- lua
+                    "lua_ls",
+                    "pyright",
+                    "dockerls",
+                    "terraformls",
+                    "sqlls",
+                    "html",
+                    "eslint",
+                    "cssls",
+                    "bashls",
+                    "gopls",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			---- Language server configurations:
-			-- lua language server
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			-- python language server
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-				settings = {
-					python = {
-						analysis = {
-							typeCheckingMode = "off",
-							reportUnusedFunction = "off",
-						},
-					},
-				},
-			})
-			-- docker language server
-			lspconfig.dockerls.setup({ capabilities = capabilities })
-			-- html-lsp
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				configurationSection = { "html", "css", "javascript" },
-				embeddedLanguages = {
-					css = true,
-					javascript = true,
-				},
-				provideFormatter = true,
-			})
-			-- eslint language server
-			lspconfig.eslint.setup({ capabilities = capabilities })
-			-- css language server
-			lspconfig.cssls.setup({ capabilities = capabilities })
-			-- terraform language server
-			lspconfig.terraformls.setup({ capabilities = capabilities })
-			-- sql language server
-			lspconfig.sqlls.setup({ capabilities = capabilities })
-			-- bash language server
-			lspconfig.bashls.setup({
-				filetypes = { "sh", "zsh", "bash" },
-				capabilities = capabilities,
-			})
-			-- go language server
-			lspconfig.gopls.setup({ capabilities = capabilities })
+            local lspconfig = require("lspconfig")
+            ---- Language server configurations:
+            -- lua language server
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            -- python language server
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+                settings = {
+                    python = {
+                        analysis = {
+                            typeCheckingMode = "off",
+                            reportUnusedFunction = "off",
+                        },
+                    },
+                },
+            })
+            -- docker language server
+            lspconfig.dockerls.setup({ capabilities = capabilities })
+            -- html-lsp
+            lspconfig.html.setup({
+                capabilities = capabilities,
+                configurationSection = { "html", "css", "javascript" },
+                embeddedLanguages = {
+                    css = true,
+                    javascript = true,
+                },
+                provideFormatter = true,
+            })
+            -- eslint language server
+            lspconfig.eslint.setup({ capabilities = capabilities })
+            -- css language server
+            lspconfig.cssls.setup({ capabilities = capabilities })
+            -- terraform language server
+            lspconfig.terraformls.setup({ capabilities = capabilities })
+            -- sql language server
+            lspconfig.sqlls.setup({ capabilities = capabilities })
+            -- bash language server
+            lspconfig.bashls.setup({
+                filetypes = { "sh", "zsh", "bash" },
+                capabilities = capabilities,
+            })
+            -- go language server
+            lspconfig.gopls.setup({ capabilities = capabilities })
 
-			---- Keymaps:
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover doc" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-			vim.keymap.set("n", "<leader>la", ":lua vim.lsp.buf.code_action()<CR>", { desc = "Code Action" })
-			vim.keymap.set("n", "<leader>lr", ":lua vim.lsp.buf.rename()<CR>", { desc = "Rename Symbol" })
-			vim.keymap.set("n", "<leader>lf", ":lua vim.lsp.buf.references()<CR>", { desc = "Find References" })
-			vim.keymap.set("n", "<leader>le", ":lua vim.diagnostic.open_float()<CR>", { desc = "Show Diagnostics" })
-			vim.keymap.set("n", "<leader>ln", ":lua vim.diagnostic.goto_next()<CR>", { desc = "Next Diagnostic" })
-			vim.keymap.set("n", "<leader>lp", ":lua vim.diagnostic.goto_prev()<CR>", { desc = "Previous Diagnostic" })
-			vim.keymap.set("n", "<leader>lq", ":Telescope diagnostics<CR>", { desc = "Search Diagnostics" })
+            ---- Keymaps:
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover doc" })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+            vim.keymap.set("n", "<leader>la", ":lua vim.lsp.buf.code_action()<CR>", { desc = "Code Action" })
+            vim.keymap.set("n", "<leader>lr", ":lua vim.lsp.buf.rename()<CR>", { desc = "Rename Symbol" })
+            vim.keymap.set("n", "<leader>lf", ":lua vim.lsp.buf.references()<CR>", { desc = "Find References" })
+            vim.keymap.set("n", "<leader>le", ":lua vim.diagnostic.open_float()<CR>", { desc = "Show Diagnostics" })
+            vim.keymap.set("n", "<leader>ln", ":lua vim.diagnostic.goto_next()<CR>", { desc = "Next Diagnostic" })
+            vim.keymap.set("n", "<leader>lp", ":lua vim.diagnostic.goto_prev()<CR>", { desc = "Previous Diagnostic" })
+            vim.keymap.set("n", "<leader>lq", ":Telescope diagnostics<CR>", { desc = "Search Diagnostics" })
 
-			vim.cmd.anoremenu("Popup.Definition <Cmd>:lua vim.lsp.buf.definition()<CR>")
-		end,
-	},
+            vim.cmd.anoremenu("Popup.Definition <Cmd>:lua vim.lsp.buf.definition()<CR>")
+        end,
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        dependencies = { "nvimtools/none-ls.nvim", "williamboman/mason.nvim" },
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("mason-null-ls").setup({
+                ensure_installed = {
+                    "stylua",
+                    "black",
+                    --"sqlfmt",
+                    "shfmt",
+                },
+                automatic_installation = true,
+            })
+        end,
+    },
+    {
+        "mfussenegger/nvim-lint",
+        config = function()
+            require("lint").linters_by_ft = {
+                go = { "golangci-lint" },
+            }
+        end,
+    },
+    {
+        "rshkarin/mason-nvim-lint",
+        config = function()
+            require("mason-nvim-lint").setup({
+                ensure_installed = { "golangci-lint" },
+                ignore_install = { "custom-linter" },
+                quiet_mode = true,
+            })
+        end,
+    },
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,39 +1,24 @@
 return {
-
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.shfmt.with({
-                        filetypes = { "sh", "zsh", "bash" },
-                    }),
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.formatting.black.with({
-						extra_args = { "--line-length", "120" },
-					}),
-					null_ls.builtins.formatting.isort,
-                    null_ls.builtins.formatting.sqlformat,
-                    null_ls.builtins.formatting.golangci_lint,
-				},
-            })
-			local function call_formatter()
-				vim.lsp.buf.format({ timeout_ms = 5000 })
-			end
-			vim.keymap.set("n", "<leader>m", call_formatter, { desc = "Format" })
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		dependencies = { "nvimtools/none-ls.nvim", "williamboman/mason.nvim" },
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = nil,
-				automatic_installation = true,
-			})
-		end,
-	},
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.shfmt.with({
+                    filetypes = { "sh", "zsh", "bash" },
+                }),
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black.with({
+                    extra_args = { "--line-length", "120" },
+                }),
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.sqlfmt,
+            },
+        })
+        local function call_formatter()
+            vim.lsp.buf.format({ timeout_ms = 5000 })
+        end
+        vim.keymap.set("n", "<leader>m", call_formatter, { desc = "Format" })
+    end,
 }
