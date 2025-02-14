30f269dd1d736acf7aff654f5f7884c016b53349
Taaniel Jakobson <taaniel.jakobson@zendesk.com>
2025-02-14 23:06:27 +0200

    updated git and autopair

--- nvim/lua/plugins/autopair.lua
+++ nvim/lua/plugins/autopair.lua
@@ -1,17 +1,17 @@
 return {
 	{
 		"windwp/nvim-autopairs",
+		dependencies = { "hrsh7th/nvim-cmp" },
 		event = "InsertEnter",
 		opts = {
 			disable_in_macro = true,
 			disable_in_replace_mode = true,
 		},
-		config = true,
-	},
-	{
-		"kylechui/nvim-surround",
 		config = function()
-			require("nvim-surround").setup({})
+			require("nvim-autopairs").setup()
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},
 }
