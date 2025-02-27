68c28c2ba293f010587a729756f90d9e75081bfa
Taaniel Jakobson <taaniel.jakobson@zendesk.com>
2025-02-27 16:57:51 +0200

    Added neogit

--- nvim/lua/plugins/git-integration.lua
+++ nvim/lua/plugins/git-integration.lua
@@ -1,75 +1,84 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
+    {
+        "lewis6991/gitsigns.nvim",
 
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { text = "✚" },
-					change = { text = "┃" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "┆" },
-				},
-				on_attach = function(bufnr)
-					local gitsigns = require("gitsigns")
+        config = function()
+            require("gitsigns").setup({
+                signs = {
+                    add = { text = "✚" },
+                    change = { text = "┃" },
+                    delete = { text = "_" },
+                    topdelete = { text = "‾" },
+                    changedelete = { text = "~" },
+                    untracked = { text = "┆" },
+                },
+                on_attach = function(bufnr)
+                    local gitsigns = require("gitsigns")
 
-					local function map(mode, l, r, opts)
-						opts = opts or {}
-						opts.buffer = bufnr
-						vim.keymap.set(mode, l, r, opts)
-					end
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
 
-					-- Navigation
-					map("n", "]c", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "]c", bang = true })
-						else
-							gitsigns.nav_hunk("next")
-						end
-					end, { desc = "Jump to next git [c]hange" })
+                    -- Navigation
+                    map("n", "]c", function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ "]c", bang = true })
+                        else
+                            gitsigns.nav_hunk("next")
+                        end
+                    end, { desc = "Jump to next git [c]hange" })
 
-					map("n", "[c", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "[c", bang = true })
-						else
-							gitsigns.nav_hunk("prev")
-						end
-					end, { desc = "Jump to previous git [c]hange" })
+                    map("n", "[c", function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ "[c", bang = true })
+                        else
+                            gitsigns.nav_hunk("prev")
+                        end
+                    end, { desc = "Jump to previous git [c]hange" })
 
-					-- Actions
-					-- visual mode
-					map("v", "<leader>gs", function()
-						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end, { desc = "git [s]tage hunk" })
-					map("v", "<leader>gr", function()
-						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end, { desc = "git [r]eset hunk" })
-					-- normal mode
-					map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Stage hunk" })
-					map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "Stage buffer" })
-					map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Reset hunk" })
-					map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Reset buffer" })
-					map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview hunk" })
-					map("n", "<leader>gb", gitsigns.blame_line, { desc = "Toggle blame" })
-					map("n", "<leader>gd", gitsigns.diffthis, { desc = "Diff against base" })
-					map("n", "<leader>gl", function()
-						gitsigns.diffthis("@")
-					end, { desc = "Diff last commit" })
-					-- Toggles
-					map(
-						"n",
-						"<leader>gb",
-						gitsigns.toggle_current_line_blame,
-						{ desc = "[T]oggle git show [b]lame line" }
-					)
-					map("n", "<leader>gD", gitsigns.toggle_linehl, { desc = "Diff line highlighting" })
-				end,
-			})
-		end,
-	},
-	{
-		"tpope/vim-fugitive",
-	},
+                    -- Actions
+                    -- visual mode
+                    map("v", "<leader>gs", function()
+                        gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end, { desc = "git [s]tage hunk" })
+                    map("v", "<leader>gr", function()
+                        gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end, { desc = "git [r]eset hunk" })
+                    -- normal mode
+                    map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+                    map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+                    map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+                    map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+                    map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+                    map("n", "<leader>gb", gitsigns.blame_line, { desc = "Toggle blame" })
+                    map("n", "<leader>gd", gitsigns.diffthis, { desc = "Diff against base" })
+                    map("n", "<leader>gl", function()
+                        gitsigns.diffthis("@")
+                    end, { desc = "Diff last commit" })
+                    -- Toggles
+                    map(
+                        "n",
+                        "<leader>gb",
+                        gitsigns.toggle_current_line_blame,
+                        { desc = "[T]oggle git show [b]lame line" }
+                    )
+                    map("n", "<leader>gD", gitsigns.toggle_linehl, { desc = "Diff line highlighting" })
+                end,
+            })
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        "neogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = true,
+    },
 }
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,78 +1,79 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set(
-			"n",
-			"<leader>e",
-			":Neotree filesystem reveal left toggle=true<CR>",
-			{ desc = "Show Files", silent = true }
-		)
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        vim.keymap.set(
+            "n",
+            "<leader>e",
+            ":Neotree filesystem reveal left toggle=true<CR>",
+            { desc = "Show Files", silent = true }
+        )
 
-		require("neo-tree").setup({
-			enable_git_status = true,
-			enable_diagnostics = true,
-			use_libuv_file_watcher = true,
-			follow_current_file = {
-				enabled = true,
-			},
-			default_component_configs = {
-				git_status = {
-					symbols = {
-						-- Change type
-						added = "✚", -- NOTE: you can set any of these to an empty string to not show them
-						deleted = "✖",
-						modified = "",
-						renamed = "➜",
-						-- Status type
-						untracked = "",
-						ignored = "◌",
-						unstaged = "",
-						staged = "✓",
-						conflict = "",
-					},
-					window = {
-						position = "float",
-						mappings = {
-							["A"] = "git_add_all",
-							["gu"] = "git_unstage_file",
-							["ga"] = "git_add_file",
-							["gr"] = "git_revert_file",
-							["gc"] = "git_commit",
-							["gA"] = "git_amend",
-							["gP"] = "git_push",
-							["gp"] = "git_pull",
-							["g?"] = "git_help",
-						},
-					},
-					align = "right",
-				},
-			},
-			window = {
-				mappings = {
-					["<space>"] = "none",
-				},
-			},
-			filesystem = {
-				group_empty_dirs = true, -- Group empty directories together
-				scan_mode = "deep",
-				use_libuv_file_watcher = true,
-				filtered_items = {
-					hide_gitignored = false,
-					always_show = {
-						".gitignore",
-						".github",
-					},
-					always_show_by_pattern = {
-						".env*",
-					},
-				},
-			},
-		})
-	end,
+        require("neo-tree").setup({
+            enable_git_status = true,
+            enable_diagnostics = true,
+            use_libuv_file_watcher = true,
+            follow_current_file = {
+                enabled = true,
+            },
+            default_component_configs = {
+                git_status = {
+                    symbols = {
+                        -- Change type
+                        added = "✚", -- NOTE: you can set any of these to an empty string to not show them
+                        deleted = "✖",
+                        modified = "",
+                        renamed = "➜",
+                        -- Status type
+                        untracked = "",
+                        ignored = "◌",
+                        unstaged = "",
+                        staged = "✓",
+                        conflict = "",
+                    },
+                    window = {
+                        position = "float",
+                        mappings = {
+                            ["A"] = "git_add_all",
+                            ["gu"] = "git_unstage_file",
+                            ["ga"] = "git_add_file",
+                            ["gr"] = "git_revert_file",
+                            ["gc"] = "git_commit",
+                            ["gA"] = "git_amend",
+                            ["gP"] = "git_push",
+                            ["gp"] = "git_pull",
+                            ["g?"] = "git_help",
+                        },
+                    },
+                    align = "right",
+                },
+            },
+            window = {
+                mappings = {
+                    ["<space>"] = "none",
+                },
+            },
+            filesystem = {
+                group_empty_dirs = true, -- Group empty directories together
+                scan_mode = "deep",
+                use_libuv_file_watcher = true,
+                filtered_items = {
+                    hide_gitignored = false,
+                    always_show = {
+                        ".github",
+                    },
+                    always_show_by_pattern = {
+                        ".env*",
+                        ".*ignore",
+                    },
+                },
+            },
+        })
+        vim.api.nvim_set_hl(0, "NeoTreeDotfile", { fg = "#aaaaaa", bg = nil, italic = false })
+    end,
 }
