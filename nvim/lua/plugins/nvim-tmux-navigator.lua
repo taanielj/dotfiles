5469166778499c083a63d2d28705fd88f6cc8cfe
Taaniel Jakobson <taaniel.jakobson@zendesk.com>
2025-05-31 08:33:56 +0300

    prevent tmuxnavigator when terminal is open

--- nvim/lua/plugins/nvim-tmux-navigator.lua
+++ nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -1,7 +1,15 @@
-return {
-	"christoomey/vim-tmux-navigator",
-	vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true }),
-	vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true }),
-	vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true }),
-	vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true }),
-}
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    -- Unmap tmux-navigator keys inside terminal buffers
+    vim.keymap.del("t", "<C-h>")
+    vim.keymap.del("t", "<C-j>")
+    vim.keymap.del("t", "<C-k>")
+    vim.keymap.del("t", "<C-l>")
+  end,
+})
+
+-- Set mappings only in normal mode
+vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -30,8 +30,8 @@ select-word-style bash
 autoload -Uz history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
-bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
 
 
 ### ────────────────────────────────
