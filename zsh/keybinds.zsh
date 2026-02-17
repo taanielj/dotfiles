8d33ab501326737d077262245277009a5dc0d435
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2026-02-17 12:52:35 +0200

    Zsh: add Home/End bindings and vim edit-command-line

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -1,4 +1,15 @@
 # Key bindings configuration
+## Edit command line in $EDITOR (neovim)
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd 'v' edit-command-line
+
+## Home and End
+bindkey "^[OH" beginning-of-line    # Home (application mode)
+bindkey "^[OF" end-of-line          # End (application mode)
+bindkey "^[[H" beginning-of-line    # Home (normal mode)
+bindkey "^[[F" end-of-line          # End (normal mode)
+
 ## ctrl up and down do nothing
 bindkey "^[[1;5A" up-line-or-history
 bindkey "^[[1;5B" down-line-or-history
