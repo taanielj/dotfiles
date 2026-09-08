84ef28848f8e4c376cbf9319b36b3f55dfecbeb6
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2026-09-08 11:04:54 +0300

    Turn on inline blame and add a name-and-date column

--- nvim/lua/plugins/editor/git-integration.lua
+++ nvim/lua/plugins/editor/git-integration.lua
@@ -12,6 +12,8 @@ return {
                     changedelete = { text = "~" },
                     untracked = { text = "┆" },
                 },
+                current_line_blame = true,
+                blame_formatter = "<author> <author_time:%Y-%m-%d>",
                 on_attach = function(bufnr)
                     local gitsigns = require("gitsigns")
 
@@ -53,7 +55,6 @@ return {
                     map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Reset hunk" })
                     map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Reset buffer" })
                     map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview hunk" })
-                    map("n", "<leader>gb", gitsigns.blame_line, { desc = "Blame line (popup)" })
                     map("n", "<leader>gd", gitsigns.diffthis, { desc = "Diff against base" })
                     map("n", "<leader>gl", function()
                         ---@diagnostic disable-next-line: param-type-mismatch
@@ -66,7 +67,22 @@ return {
                     end, { desc = "Diff against main/master branch" })
 
                     -- Toggles
-                    map("n", "<leader>gB", gitsigns.toggle_current_line_blame, { desc = "Toggle git blame line" })
+                    local function toggle_blame_column()
+                        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+                            if vim.bo[vim.api.nvim_win_get_buf(win)].filetype == "gitsigns-blame" then
+                                vim.api.nvim_win_close(win, true)
+                                return
+                            end
+                        end
+                        local win = vim.api.nvim_get_current_win()
+                        gitsigns.blame(nil, function()
+                            if vim.api.nvim_win_is_valid(win) then
+                                vim.api.nvim_set_current_win(win)
+                            end
+                        end)
+                    end
+                    map("n", "<leader>gb", gitsigns.toggle_current_line_blame, { desc = "Toggle inline blame" })
+                    map("n", "<leader>gB", toggle_blame_column, { desc = "Toggle blame column" })
                     map("n", "<leader>gD", gitsigns.toggle_linehl, { desc = "Diff line highlighting" })
 
                     -- Open current line in browser (GitHub/GitLab/Bitbucket)
