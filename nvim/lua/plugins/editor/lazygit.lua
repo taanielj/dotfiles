48463fca6873b83e1fe6d5bfcf5d2a857b32d99f
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2026-09-08 20:27:56 +0300

    Lazy-load lazygit.nvim on its commands and keymap

--- nvim/lua/plugins/editor/lazygit.lua
+++ nvim/lua/plugins/editor/lazygit.lua
@@ -4,11 +4,12 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope.nvim",
     },
+    cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
+    keys = {
+        { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
     config = function()
         require("telescope").load_extension("lazygit")
-        vim.keymap.set("n", "<leader>gg", function()
-            vim.cmd("LazyGit")
-        end, { desc = "LazyGit" })
         vim.g.lazygit_on_exit_callback = function()
             local state = require("neo-tree.sources.manager").get_state("filesystem")
             require("neo-tree.sources.filesystem.commands").refresh(state)
