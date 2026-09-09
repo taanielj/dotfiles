6b056059da10ff6a08982f9379ddc7b8d7dbb01d
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2026-09-09 15:40:07 +0300

    Declare the yank maps as rows like the other keybinds

--- nvim/lua/keybinds/yank.lua
+++ nvim/lua/keybinds/yank.lua
@@ -6,41 +6,28 @@ local function line_suffix()
     return first == last and (":" .. first) or (":" .. first .. "-" .. last)
 end
 
-vim.keymap.set(
-    "n",
-    "<leader>yb",
-    function() yank.copy(vim.fn.expand("%:p"), "path") end,
-    { desc = "Yank buffer absolute path" }
-)
-
-vim.keymap.set(
-    { "n", "v" },
-    "<leader>yl",
-    function() yank.copy(vim.fn.expand("%:p") .. line_suffix(), "path:line") end,
-    { desc = "Yank buffer path with line" }
-)
-
-vim.keymap.set("n", "<leader>yh", function() require("ui.md2html").yank() end, { desc = "Yank buffer as HTML" })
-
-vim.keymap.set(
-    "v",
-    "<leader>yh",
-    function() require("ui.md2html").yank(yank.line_range()) end,
-    { desc = "Yank selection as HTML" }
-)
-
 local function remote_url() return require("lib.git").web_url(vim.fn.expand("%:p"), yank.line_range()) end
 
-vim.keymap.set({ "n", "v" }, "<leader>yg", function()
+local function yank_remote_url()
     local url = remote_url()
     if url then
         yank.copy(url, "git URL")
     end
-end, { desc = "Yank git URL for line" })
+end
 
-vim.keymap.set({ "n", "v" }, "<leader>go", function()
+local function open_remote_url()
     local url = remote_url()
     if url then
         vim.ui.open(url)
     end
-end, { desc = "Open line in browser" })
+end
+
+-- stylua: ignore
+require("lib.keymap").rows({
+    { "n",          "<leader>yb", function() yank.copy(vim.fn.expand("%:p"), "path") end,                     "Yank buffer absolute path" },
+    { { "n", "v" }, "<leader>yl", function() yank.copy(vim.fn.expand("%:p") .. line_suffix(), "path:line") end, "Yank buffer path with line" },
+    { "n",          "<leader>yh", function() require("ui.md2html").yank() end,                                  "Yank buffer as HTML" },
+    { "v",          "<leader>yh", function() require("ui.md2html").yank(yank.line_range()) end,                 "Yank selection as HTML" },
+    { { "n", "v" }, "<leader>yg", yank_remote_url,                                                              "Yank git URL for line" },
+    { { "n", "v" }, "<leader>go", open_remote_url,                                                              "Open line in browser" },
+})
