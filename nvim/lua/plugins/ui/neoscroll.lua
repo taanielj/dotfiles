ef9de9c65e3e82620fb7cbe1c5b21c95cdc3aa6b
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2026-09-09 12:34:21 +0300

    Keep neoscroll's default keys, only C-y and C-e are custom

--- nvim/lua/plugins/ui/neoscroll.lua
+++ nvim/lua/plugins/ui/neoscroll.lua
@@ -1,7 +1,8 @@
 return {
     "karb94/neoscroll.nvim",
     opts = {
-        mappings = {}, -- keybinds/view.lua owns the scroll keys
+        -- <C-y> and <C-e> are in keybinds/view.lua with their own speed
+        mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
         easing = "quadratic",
         duration_multiplier = 1,
         hide_cursor = true,
