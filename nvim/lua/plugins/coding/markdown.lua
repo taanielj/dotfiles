eec6cc528591c1a971c2c1abcb49bcc541ca8dc6
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2026-09-14 09:37:52 +0300

    Replace peek.nvim with live-preview.nvim

--- nvim/lua/plugins/coding/markdown.lua
+++ nvim/lua/plugins/coding/markdown.lua
@@ -8,32 +8,21 @@ return {
         opts = {},
     },
     {
-        "toppair/peek.nvim",
-        ft = "markdown",
-        build = "deno task --quiet build",
-        opts = {
-            -- Preview in the default browser; peek's "browser" shells out to xdg-open, absent on WSL.
-            app = vim.fn.has("wsl") == 1 and { "wslview" } or "browser",
-        },
+        "brianhuster/live-preview.nvim",
+        cmd = "LivePreview",
         keys = {
             {
                 "<leader>p",
                 function()
-                    local peek = require("peek")
-                    if peek.is_open() then
-                        peek.close()
+                    if require("livepreview").is_running() then
+                        vim.cmd("LivePreview close")
                     else
-                        peek.open()
+                        vim.cmd("LivePreview start")
                     end
                 end,
                 ft = "markdown",
                 desc = "Preview markdown",
             },
         },
-        config = function(_, opts)
-            require("peek").setup(opts)
-            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-        end,
     },
 }
